eb2d9a7888d0e323ce3ccd50b86c8cec69155e5f
mtkachenko <oblomov86@gmail.com>
2019-02-14 21:35:01 +0300

    draft project with todos

---
+++ src/dump-b/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace dump_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("provide path to dump file");
+                return;
+            }
+
+            var path = args[0];
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("file not found");
+                return;
+            }
+
+            //start processing
+            //todo 1. parse threads
+            //todo 2. generate html file
+            //todo 3. parse heap
+        }
+    }
+}
